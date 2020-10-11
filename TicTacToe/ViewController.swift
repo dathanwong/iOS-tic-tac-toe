609d7158dc04f38782155c8db09fc69e54916000
dathwo@outlook.com <dathwo@outlook.com>
2020-10-11 16:18:24 -0700

    Initial commit

---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Dathan Wong on 10/11/20.
+//  Copyright © 2020 Dathan Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
